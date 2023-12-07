88b91f826558249f5fe96f4d71b927963dfbf47a
Will <valiquettewilliam@gmail.com>
2023-12-07 11:09:25 -0500

    first commit day 1

---
+++ day1/go.mod
@@ -0,0 +1,3 @@
+module github.com/valiquettewilliam/AdventOfCode/day1
+
+go 1.21.4
---
+++ day1/main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	// re := regexp.MustCompile("[0-9]+")
+	// fmt.Println(re.FindAllString("abc123def", 0))
+}
